5b7234a5c2ed14cbe3c6178d3b5e274854b817c4
Jason Soprovich <190052169+jasonsoprovich@users.noreply.github.com>
2025-01-08 21:02:36 -0700

    feat: add pseudocode template

---
+++ script.js
@@ -0,0 +1,25 @@
+/*
+PSEUDOCODE
+START
+
+	Define variables for user input and computer input
+	
+	Ask user for to select between "Rock", "Paper" or "Scissors
+		Check to see if user entered a valid selection
+	Randomly generate computer input
+	
+	Declare function to check for win condition
+		Define variable for result
+				If user input and computer input are equal, then result equals "Tie"
+Else
+					If user input equals "Rock" and computer input equals "Paper" then result equals "Lose" 
+						Else result equals "Win"
+					If user input equals "Paper" and computer input equals "Scissors" then result equals "Lose" 
+						Else result equals "Win"
+					If user input equals "Scissors" and computer input equals "Rock" then result equals "Lose" 
+						Else result equals "Win"
+			Return result
+	Print result from invoking win condition function
+
+    END
+*/
