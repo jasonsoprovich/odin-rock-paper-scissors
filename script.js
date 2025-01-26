cc4de174c1fbaab127291d496b691a617c508150
jasonsoprovich <190052169+jasonsoprovich@users.noreply.github.com>
2025-01-26 13:50:14 -0700

    feat: add event listeners to buttons

--- script.js
+++ script.js
@@ -1,73 +1,54 @@
-function playGame() {
-	const maxRounds = 5;
-	let humanScore = 0;
-	let computerScore = 0;
-	let gameCancelled = false;
+// function playGame() {
+// 	// const maxRounds = 5;
+// 	let humanScore = 0;
+// 	let computerScore = 0;
+// 	let gameCancelled = false;
 
-	for (let i = 0; i < maxRounds; i++) {
-		const humanSelection = getHumanChoice();
+// 	// for (let i = 0; i < maxRounds; i++) {
+// 		const humanSelection = getHumanChoice();
 		
-		// end game loop if input is cancelled by user
-		if (humanSelection === null) {
-			gameCancelled = true;
-			break;
-		}
+// 		// end game loop if input is cancelled by user
+// 		if (humanSelection === null) {
+// 			gameCancelled = true;
+// 			// break;
+// 		}
 
-		const computerSelection = getComputerChoice().toLowerCase();
-		const result = playRound(humanSelection, computerSelection);
+// 		const computerSelection = getComputerChoice().toLowerCase();
+// 		const result = playRound(humanSelection, computerSelection);
 
-		// keep track of total scores
-		if (result  === "win") {
-			++humanScore;
-		} else if (result === "lose") {
-			++computerScore;
-		} else {
-			// do nothing if tied
-		}
+// 		// keep track of total scores
+// 		if (result  === "win") {
+// 			++humanScore;
+// 		} else if (result === "lose") {
+// 			++computerScore;
+// 		} else {
+// 			// do nothing if tied
+// 		}
 
-		// display result from current round
-		console.log(`Round ${i + 1}: You chose ${humanSelection.toLowerCase()} and the computer chose ${computerSelection.toLowerCase()} -  You ${result}.`);
-		console.log(`Human Score: ${humanScore} - Computer Score: ${computerScore}`);
-	}
-	// return the scores as an array since only one variable can be returned? May be a better way to do this?
-	return [humanScore, computerScore, gameCancelled];
-}
+// 		// display result from current round
+// 		// console.log(`Round ${i + 1}: You chose ${humanSelection.toLowerCase()} and the computer chose ${computerSelection.toLowerCase()} -  You ${result}.`);
+// 		console.log(`Human Score: ${humanScore} - Computer Score: ${computerScore}`);
+// 	}
+// 	// return the scores as an array since only one variable can be returned? May be a better way to do this?
+// 	// return [humanScore, computerScore, gameCancelled];
+// // }
 
-function finalResult(finalScore) {
-	const [humanScore, computerScore, gameCancelled] = finalScore; // split array
-	if (gameCancelled === true) {
-		console.log("Game was cancelled by the user.");
-		return;
-	}
+// function finalResult(finalScore) {
+// 	const [humanScore, computerScore, gameCancelled] = finalScore; // split array
+// 	if (gameCancelled === true) {
+// 		console.log("Game was cancelled by the user.");
+// 		return;
+// 	}
 
-	console.log(`Final Result - Human Score: ${humanScore} - Computer score: ${computerScore}.`);
-	if (humanScore > computerScore) {
-		console.log("You won the game.");
-	} else if (humanScore < computerScore) {
-		console.log("You lost the game.");
-	} else {
-		console.log("Tie game.");
-	}
-}
-
-function getHumanChoice() {
-	// Check if user input is valid
-	const validOptions = ["rock", "paper", "scissors"];
-	let humanChoice = "";
-	
-	while (!validOptions.includes((humanChoice || "").toLowerCase())) {
-		humanChoice = prompt("Please enter a valid selection (Rock, Paper, or Scissors):");
-	// Check if user cancels with out making any selection
-		if (humanChoice === null) {
-			return null;
-		}
-	// Invalid entry, re-select
-		if (!validOptions.includes(humanChoice.toLowerCase())) {
-			alert("Invalid entry. Please re-enter your selection.");
-		}
-	}
-	return humanChoice;
-}
+// 	console.log(`Final Result - Human Score: ${humanScore} - Computer score: ${computerScore}.`);
+// 	if (humanScore > computerScore) {
+// 		console.log("You won the game.");
+// 	} else if (humanScore < computerScore) {
+// 		console.log("You lost the game.");
+// 	} else {
+// 		console.log("Tie game.");
+// 	}
+// }
 
 function getComputerChoice() {
 	const validOptions = ["Rock", "Paper", "Scissors"];
@@ -76,38 +57,61 @@ function getComputerChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
-	let roundResult;
+	let result;
 	if (humanChoice.toLowerCase() === computerChoice.toLowerCase()) {
-		roundResult = "tied";	
+		result = "tied";	
 	} else if (humanChoice === ""){
-		roundResult = null;
+		result = null;
 	} else if (humanChoice.toLowerCase() === "rock" && computerChoice.toLowerCase() === "paper") {
-		roundResult = "lose";
+		result = "lose";
 	} else if (humanChoice.toLowerCase() === "paper" && computerChoice.toLowerCase() === "scissors") {
-		roundResult = "lose";
+		result = "lose";
 	} else if (humanChoice.toLowerCase() === "scissors" && computerChoice.toLowerCase() === "rock") {
-		roundResult = "lose";
+		result = "lose";
 	} else {
-		roundResult = "win";
+		result = "win";
 	}
-	return roundResult;
+	return result;
 }
 
-const finalScore = playGame(); // initiate the game and return the final score as an array
-finalResult(finalScore); // display final result of game using an array from playGame()
+// NEW CODE
 
 const buttons = document.getElementById('button-container');
+const results = document.getElementById('results-container');
 
 const rockButton = document.createElement('button');
 const paperButton = document.createElement('button');
 const scissorsButton = document.createElement('button');
+const result = document.createElement('h3');
 
-rockButton.textContent = 'Rock';
-paperButton.textContent = 'Paper';
-scissorsButton.textContent = 'Scissors';
+const rockClick = rockButton.textContent = 'Rock';
+const paperClick = paperButton.textContent = 'Paper';
+const scissorsClick = scissorsButton.textContent = 'Scissors';
 
 buttons.appendChild(rockButton);
 buttons.appendChild(paperButton);
 buttons.appendChild(scissorsButton);
+
+
+results.appendChild(document.createTextNode('RESULT'));
+
+rockButton.addEventListener('click', function(){
+	const computerChoice = getComputerChoice();
+	const result = playRound(rockClick, computerChoice);
+	console.log(result);
+});
+
+paperButton.addEventListener('click', function(){
+	const computerChoice = getComputerChoice();
+	const result = playRound(paperClick, computerChoice);
+	console.log(result);
+});
+
+scissorsButton.addEventListener('click', function(){
+	const computerChoice = getComputerChoice();
+	const result = playRound(scissorsClick, computerChoice);
+	console.log(result);
+});
+
 
 
